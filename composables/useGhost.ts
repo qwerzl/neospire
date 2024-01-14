78e1ee8d83fdf1d344fc99d16812d3eba49ee5cf
qwerzl <me@qwerzl.me>
2024-01-14 23:37:53 +0800

    chore: add comments

--- composables/useGhost.ts
+++ composables/useGhost.ts
@@ -3,7 +3,10 @@ import GhostContentAPI from "@tryghost/content-api";
 export const useGhost = () => {
   return new GhostContentAPI({
     url: 'http://localhost:3000/api/ghost',
-    key: 'eda34c69df85b9ddf91861f0ed',
+    // This is a fake key to fool the library. The library enforces 26 hex characters like a donkey.
+    // Real key is read from env var in server/api/ghost/[...slug].ts
+    // For safety. To get rid of those little hackers.
+    key: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
     version: "v5.0"
   })
 }
