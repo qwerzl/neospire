421263f7f8428d496255442723de11eed5910d5a
qwerzl <me@qwerzl.me>
2024-01-16 14:40:30 +0800

    feat: add site footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,27 @@
     <div class="relative top-16">
       <slot />
     </div>
+    <div class="rounded-lg shadow bg-background relative top-16 mt-6 h-32 flex justify-between border-t">
+      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <CustomNavIcon></CustomNavIcon>
+        <span class="text-sm sm:text-center text-foreground">© 2024 <a href="https://computerization.io" class="hover:underline">Computerization</a>. All Rights Reserved.
+        </span>
+          <ul class="flex flex-wrap items-center mt-3 text-sm sm:mt-0">
+            <li>
+              <!-- TODO: GitHub link -->
+              <NuxtLink href="#" class="me-4">
+                <Icon name="mdi:github" size="2em"/>
+              </NuxtLink>
+            </li>
+            <li>
+              <!-- TODO: Mail link -->
+              <NuxtLink href="#" class="me-4">
+                <Icon name="material-symbols:mail" size="2em" />
+              </NuxtLink>
+            </li>
+          </ul>
+      </div>
+    </div>
   </div>
 </template>
 
